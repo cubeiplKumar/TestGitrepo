ca6fdc1bb5ca841d543611e55ece80b841b56eda
AnilKumar <emailanilkumarm@gmail.com>
2018-01-05 10:44:31 +0530

    New files added to the program

---
+++ TestGit.java
@@ -0,0 +1,12 @@
+
+public class TestGit{
+	public static void main (String [] args){
+	   for( int i=1;i<=10;i++){
+	
+	      System.out.println("CubeIPL Corporate DevOps Training and Consulting Firm:"+i);
+	    }
+
+
+	}
+  
+}
